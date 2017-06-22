0d8e91b078cc1e9c820f5427866fc8232b5eaeb6
joyharr162 <joyharr162@ed.amdsb.ca>
2017-06-22 13:51:34 -0400

    please kill me

--- RunningGame/Screens/InstructionScreen.Designer.cs
+++ RunningGame/Screens/InstructionScreen.Designer.cs
@@ -33,7 +33,12 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.joystick = new System.Windows.Forms.PictureBox();
+            this.rightSword = new System.Windows.Forms.PictureBox();
+            this.leftSword = new System.Windows.Forms.PictureBox();
+            this.highscoreLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.joystick)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rightSword)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.leftSword)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -53,7 +58,7 @@
             this.label2.BackColor = System.Drawing.Color.SaddleBrown;
             this.label2.Location = new System.Drawing.Point(64, 121);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(792, 339);
+            this.label2.Size = new System.Drawing.Size(792, 311);
             this.label2.TabIndex = 9;
             // 
             // label3
@@ -70,7 +75,7 @@
             // 
             // joystick
             // 
-            this.joystick.BackColor = System.Drawing.Color.SaddleBrown;
+            this.joystick.BackColor = System.Drawing.Color.Transparent;
             this.joystick.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("joystick.BackgroundImage")));
             this.joystick.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.joystick.Location = new System.Drawing.Point(186, 203);
@@ -79,11 +84,48 @@
             this.joystick.TabIndex = 10;
             this.joystick.TabStop = false;
             // 
+            // rightSword
+            // 
+            this.rightSword.BackColor = System.Drawing.Color.Transparent;
+            this.rightSword.BackgroundImage = global::RunningGame.Properties.Resources.scimitarflip;
+            this.rightSword.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rightSword.Location = new System.Drawing.Point(563, 457);
+            this.rightSword.Name = "rightSword";
+            this.rightSword.Size = new System.Drawing.Size(51, 24);
+            this.rightSword.TabIndex = 17;
+            this.rightSword.TabStop = false;
+            // 
+            // leftSword
+            // 
+            this.leftSword.BackColor = System.Drawing.Color.Transparent;
+            this.leftSword.BackgroundImage = global::RunningGame.Properties.Resources.scimitar;
+            this.leftSword.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.leftSword.Location = new System.Drawing.Point(286, 457);
+            this.leftSword.Name = "leftSword";
+            this.leftSword.Size = new System.Drawing.Size(51, 24);
+            this.leftSword.TabIndex = 16;
+            this.leftSword.TabStop = false;
+            // 
+            // highscoreLabel
+            // 
+            this.highscoreLabel.AutoSize = true;
+            this.highscoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.highscoreLabel.Font = new System.Drawing.Font("Arial", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.highscoreLabel.ForeColor = System.Drawing.Color.DarkGray;
+            this.highscoreLabel.Location = new System.Drawing.Point(342, 449);
+            this.highscoreLabel.Name = "highscoreLabel";
+            this.highscoreLabel.Size = new System.Drawing.Size(215, 40);
+            this.highscoreLabel.TabIndex = 15;
+            this.highscoreLabel.Text = "MAIN MENU";
+            // 
             // InstructionScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
+            this.Controls.Add(this.rightSword);
+            this.Controls.Add(this.leftSword);
+            this.Controls.Add(this.highscoreLabel);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.joystick);
             this.Controls.Add(this.label2);
@@ -92,6 +134,8 @@
             this.Size = new System.Drawing.Size(900, 500);
             this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.InstructionScreen_PreviewKeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.joystick)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rightSword)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.leftSword)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -103,5 +147,8 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.PictureBox joystick;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox rightSword;
+        private System.Windows.Forms.PictureBox leftSword;
+        private System.Windows.Forms.Label highscoreLabel;
     }
 }
---
+++ RunningGame/Screens/PauseScreen.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace RunningGame.Screens
+{
+    partial class PauseScreen
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pauseBackground = new System.Windows.Forms.Label();
+            this.quitLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // pauseBackground
+            // 
+            this.pauseBackground.BackColor = System.Drawing.Color.Black;
+            this.pauseBackground.Location = new System.Drawing.Point(7, 7);
+            this.pauseBackground.Name = "pauseBackground";
+            this.pauseBackground.Size = new System.Drawing.Size(286, 136);
+            this.pauseBackground.TabIndex = 0;
+            this.pauseBackground.Text = "label1";
+            // 
+            // quitLabel
+            // 
+            this.quitLabel.AutoSize = true;
+            this.quitLabel.BackColor = System.Drawing.Color.Black;
+            this.quitLabel.Font = new System.Drawing.Font("Segoe Script", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quitLabel.ForeColor = System.Drawing.Color.White;
+            this.quitLabel.Location = new System.Drawing.Point(89, 13);
+            this.quitLabel.Name = "quitLabel";
+            this.quitLabel.Size = new System.Drawing.Size(101, 44);
+            this.quitLabel.TabIndex = 1;
+            this.quitLabel.Text = "Quit?";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Segoe Script", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(60, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 40);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Yes";
+            // 
+            // PauseScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.quitLabel);
+            this.Controls.Add(this.pauseBackground);
+            this.Name = "PauseScreen";
+            this.Size = new System.Drawing.Size(300, 150);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label pauseBackground;
+        private System.Windows.Forms.Label quitLabel;
+        private System.Windows.Forms.Label label1;
+    }
+}
